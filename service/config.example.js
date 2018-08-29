7ca4648c6ddc3207796cc0b318b299e2c18955f9
pstrawberrie <github_main@pstrawberrie.live>
2018-08-29 07:55:28 -0600

    removed info from config example

--- service/config.example.js
+++ service/config.example.js
@@ -1,9 +1,9 @@
 module.exports = {
   devMode: true,
-  twitchUsername: 'pstrawberrieb0t',
-  twitchChannels: ['pstrawberrie'],
-  botOwner: 'pstrawberrie',
-  twitchMainChannel: 'pstrawberrie',
-  twitchKey: 'oauth:ijkseubhi6j7ethpt6vchdw12a6gln',
+  twitchUsername: 'botnamehere',
+  twitchChannels: ['botchannel'],
+  botOwner: 'botownername',
+  twitchMainChannel: 'botmainchannel',
+  twitchKey: 'oauth:xxx',
   db: 'mongodb://localhost:27017/phoenix'
 }
